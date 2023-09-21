86f55f628f0570d2691e6b017731acc295610a4c
adnankhayrou <adnanekhayrou3@gmail.com>
2023-09-21 11:59:22 +0100

    adding check winner function and conditions in handle click function for check the winner or it's a draw and switch between x and o

--- script.js
+++ script.js
@@ -10,30 +10,80 @@ let currentPlayer = "X";
 let gameOver = false;
 let playerXWins = 0;
 let playerOWins = 0;
+let movesMade = 0;
 
 // game board
 GameBoard();
 
+function checkWinner(row, col) {
+
+    // Check horizontal
+    for (let j = 0; j < 16; j++) {
+        if(boardState[row][j]=== currentPlayer &&
+            boardState [row][j + 1] === currentPlayer &&
+            boardState [row][j + 2] === currentPlayer &&
+            boardState [row][j + 3] === currentPlayer &&
+            boardState [row][j + 4] === currentPlayer
+        ){
+            return true;
+        }
+    }
+    // Check vertical
+    for (let j = 0; j < 16; j++) {
+            if(boardState[j][col]=== currentPlayer &&
+                boardState [j+1][col] === currentPlayer &&
+                boardState [j+2][col] === currentPlayer &&
+                boardState [j+3][col] === currentPlayer &&
+                boardState [j+4][col] === currentPlayer
+            ){
+                return true;
+            }
+    }
+
+    return false;
+}
+
 function handleClick(e) {
     const row = e.target.dataset.row;
     const col = e.target.dataset.col;
+    
     if (boardState[row][col] == "" && gameOver == false) {
         boardState[row][col] = currentPlayer;
         e.target.textContent = currentPlayer;
+        movesMade++;
+        console.log(movesMade);
+
+        if (checkWinner(row, col)) {
+            alert(`Player ${currentPlayer} wins!`);
+            if (currentPlayer === "X") {
+                playerXWins++;
+                playerXWinsElement.textContent = playerXWins;
+            } else {
+                playerOWins++;
+                playerOWinsElement.textContent = playerOWins;
+            }
+            gameOver = true;
+        } else if (movesMade == 399) {
+            message.textContent = "It's a draw!";
+            gameOver = true;
+        } else {
+            currentPlayer = currentPlayer === "X" ? "O" : "X";
+            message.textContent = `Player ${currentPlayer} turn`;
+        }
+
     }
 }
-
 
 
 function GameBoard(){
     for (let i = 0; i < 20; i++) {
         for(let j = 0; j < 20; j++){
-        const cell = document.createElement("div");
-        cell.classList.add("cell");
-        cell.dataset.row = i;
-        cell.dataset.col = j;
-        board.appendChild(cell);
-        cell.addEventListener("click", handleClick);
+            const cell = document.createElement("div");
+            cell.classList.add("cell");
+            cell.dataset.row = i;
+            cell.dataset.col = j;
+            board.appendChild(cell);
+            cell.addEventListener("click", handleClick);
         }
     }
 }
